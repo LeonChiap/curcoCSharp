22284ccf6d8d120e50ba657f7a85175799d0a708
Leon Chiappa <leonchandroid@gmail.com>
2020-09-11 22:54:10 -0300

    Inicializando git en el proyecto

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace coreEscuela
+{
+    class coreEscuela
+    {
+        string nombre;
+        string direccion;
+        int añoFundacion;
+
+        void timbrar()
+        { 
+            Console.Beep(1000,3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
